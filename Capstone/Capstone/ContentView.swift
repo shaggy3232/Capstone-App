e713aae27b986e36b18039b1f0138f43483872b4
shaggy3232 <sbrem044@uottawa.ca>
2020-09-23 09:18:05 -0400

    init commit

---
+++ Capstone/Capstone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Capstone
+//
+//  Created by Shaggy Bremnath on 2020-09-23.
+//  Copyright © 2020 Shaggy Bremnath. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
