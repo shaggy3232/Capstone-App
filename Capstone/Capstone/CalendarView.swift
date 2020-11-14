d4849b5f15a255f44fcfc77c086c1fa189044c84
sheh <shehryaramalik@gmail.com>
2020-11-14 14:20:54 -0500

    CalenderInit

---
+++ Capstone/Capstone/CalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  Capstone
+//
+//  Created by Shehryar Malik on 2020-11-14.
+//  Copyright © 2020 Shaggy Bremnath. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
